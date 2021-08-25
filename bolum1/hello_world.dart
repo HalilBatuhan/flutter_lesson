318dc4e18eeb27bf7c9c55848ce74537f9ef0e59
HalilBatuhan <67440436+HalilBatuhan@users.noreply.github.com>
2021-08-25 03:27:39 +0300

    25.08.21

---
+++ bolum1/hello_world.dart
@@ -0,0 +1,5 @@
+main(List<String> args) {
+  print(545461231234654 + 45654545);
+  print("Dart ögreniyom flutter değil altını çizerim");
+  print('Sa Halil\'in');
+}
---
+++ bolum1/veritipleri_degiskenler.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) {
+  /* String Değişkeni */
+  print("---------------------");
+  String name = "Halil";
+  String renk = "sarı";
+  print(name);
+  print(name + " bir öğrenci");
+  print(name + "'nin en sevdiği renk" + renk);
+/* İnt,Num,Double değişkeni */
+  print("---------------------");
+  int age = 16;
+  num iq = 1.2;
+  double ortalama = 12312.123;
+
+  print(age);
+  print(iq);
+  print(ortalama);
+/* Bool Değişkeni  */
+  print("---------------------");
+  bool ucuyorMu = false;
+  bool yasiyorMu = true;
+
+  print(ucuyorMu);
+  print(yasiyorMu);
+/* Var Değişkeni */
+  print("---------------------");
+  var SchoolName = "GöksüAnadoluLisesi";
+  var SchoolNumber = 192;
+  var Teacher = false;
+
+  print(SchoolName);
+  print(SchoolNumber);
+  print(Teacher);
+}
